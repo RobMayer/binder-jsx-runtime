6435aba65e3a051330f17a19b5791003854d1d21
RobMayer <5171361+RobMayer@users.noreply.github.com>
2021-06-07 21:32:47 -0400

    update

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "DeferredElement", {
+  enumerable: true,
+  get: function get() {
+    return _jsxRuntime.DeferredElement;
+  }
+});
+Object.defineProperty(exports, "render", {
+  enumerable: true,
+  get: function get() {
+    return _jsxRuntime.render;
+  }
+});
 exports["default"] = void 0;
 
-var _jsxRuntime = _interopRequireDefault(require("./jsx-runtime"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var _jsxRuntime = require("./jsx-runtime");
 
 var _default = {
-  runtime: _jsxRuntime["default"]
+  DeferredElement: _jsxRuntime.DeferredElement,
+  render: _jsxRuntime.render
 };
 exports["default"] = _default;
